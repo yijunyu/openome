268a8e9c515f9c10c1eab2bf0b09af3a36cef1f2
yijun <yijun@1ab8f05b-f909-0410-947c-871aca028256>
2006-01-18 23:07:12 +0000

    Moved remotely
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@24 1ab8f05b-f909-0410-947c-871aca028256

---
+++ it.unitn.goal_analysis/src/it/unitn/goal_analysis/graph_creation/ContinuousLabel.java
@@ -0,0 +1,123 @@
+package it.unitn.goal_analysis.graph_creation; 
+import java.util.MissingResourceException;
+
+/**
+ * This class implements a label for a node that can take continuous 
+ * values; it extends the base class Label [or is it an interface? and 
+ * what does it specify? ]
+ */
+
+
+class ContinuousLabel extends Label implements Cloneable {
+    private final float myValue;      // for easy comparisons between labels
+    private final String myName;    // name of the label, for its displaying
+
+	/**
+	 * 
+	 * @uml.property name="nO"
+	 * @uml.associationEnd multiplicity="(0 1)"
+	 */
+	// static objects ...
+	static final ContinuousLabel NO = new ContinuousLabel(0.0f);
+
+	/**
+	 * 
+	 * @uml.property name="pARTIAL"
+	 * @uml.associationEnd multiplicity="(0 1)"
+	 */
+	static final ContinuousLabel PARTIAL = new ContinuousLabel(0.5f);
+
+	/**
+	 * 
+	 * @uml.property name="tOTAL"
+	 * @uml.associationEnd multiplicity="(0 1)"
+	 */
+	static final ContinuousLabel TOTAL = new ContinuousLabel(1.0f);
+
+    /* This constructor takes a string that represents the label as 
+     * seen by the client
+     * impossible to create two labels with the same numeric value... 
+     * of course it is still possible to create two labels with different
+     * numeric value but identical name, resulting in a complete 
+     * mess in the results. What should be the access modifier 
+     * for this constructor?    
+     */
+    private ContinuousLabel(float aValue) {  
+	myValue = aValue;
+	myName = String.valueOf(myValue);
+    }
+
+    protected Object clone() 
+	throws CloneNotSupportedException {    
+	return this;
+    }
+   
+    /* Access method for the name, it should return a const....
+     * @return the name as a string
+     */
+    public String getName() {
+	return myName;
+    }
+
+    /**
+     * Access method for the value, hides retrieval
+     * @return the value as an int
+     */
+    private float getValue() {
+	return myValue;
+    }
+
+    /* Comparison method
+     */
+    
+    public int compareTo(Object aLabel) {
+	ContinuousLabel tempLabel = (ContinuousLabel) aLabel; // ????
+	// now use the comparison between floats
+	float diff = tempLabel.getValue() - this.getValue(); 
+	if (Math.abs(diff) < Float.MIN_VALUE)
+	    return 0;
+	else if (diff > 0.0) 
+	    return -1;
+	else return 1;
+    }
+    
+    static Label findLabel(String name) 
+	throws MissingResourceException {
+	if (name.compareTo("MINIMUM") == 0) return NO;
+	if (name.compareTo("NO")      == 0) return NO;
+	if (name.compareTo("PARTIAL") == 0) return PARTIAL;
+	if (name.compareTo("TOTAL")   == 0) return TOTAL;
+	if (name.compareTo("0.0")     == 0) return NO;
+	if (name.compareTo("1.0")     == 0) return TOTAL;
+	if (name.compareTo("0.5")     == 0) return PARTIAL;
+
+	float value = Float.parseFloat(name);
+	if (value < 0.0 || value > 1.0 )
+	    throw new MissingResourceException("Label not in [0,1]", 
+					       " ContinuousLabel ",
+					       name);
+	return new ContinuousLabel(value); 
+    }
+ 
+    ContinuousLabel sum (final ContinuousLabel aLabel) {
+	float p1 = this.getValue();
+	float p2 = aLabel.getValue();
+	return ((ContinuousLabel)findLabel("" + (p1 + p2 - p1 * p2))); 
+    }
+
+
+    ContinuousLabel product (final ContinuousLabel aLabel) {
+	float p1 = this.getValue();
+	float p2 = aLabel.getValue();
+	return ((ContinuousLabel)findLabel("" + (p1 * p2))); 
+    }
+
+
+    ContinuousLabel product (final float aWeight) {
+	float p1 = this.getValue();
+	return ((ContinuousLabel)findLabel("" + (p1 * aWeight))); 
+    }
+
+
+    
+}
---
+++ it.unitn.goal_analysis/src/it/unitn/goal_analysis/graph_creation/DiscreteRelType.java
@@ -0,0 +1,199 @@
+package it.unitn.goal_analysis.graph_creation; 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.MissingResourceException;
+
+/**
+ * This class describes a relation type, namely, a discrete relation type; 
+ * it inherits from the RelType class and delegates the solution for a 
+ * relation to the Solver class. It actually contains two such solvers 
+ * because we have decided to split the propagation for the S and the D 
+ * labels
+ */
+
+
+
+class DiscreteRelType extends RelType {
+
+	/**
+	 * 
+	 * @uml.property name="satSolver"
+	 * @uml.associationEnd multiplicity="(0 1)"
+	 */
+	private final Solver satSolver; // solver for the sat. side of propagation
+
+	/**
+	 * 
+	 * @uml.property name="denSolver"
+	 * @uml.associationEnd multiplicity="(0 1)"
+	 */
+	private final Solver denSolver; // solver for the den. side of propagation
+
+    private final String myName;
+    // keep track of the (static) objects created
+    private static LinkedList relTypePool = new LinkedList();
+
+	/**
+	 * 
+	 * @uml.property name="aND"
+	 * @uml.associationEnd multiplicity="(0 1)"
+	 */
+	// static objects!!
+	static final DiscreteRelType AND = new DiscreteRelType(
+		new DiscreteCalcMinSolver(false),
+		new DiscreteCalcMaxSolver(true),
+		"AND");
+
+	/**
+	 * 
+	 * @uml.property name="oR"
+	 * @uml.associationEnd multiplicity="(0 1)"
+	 */
+	static final DiscreteRelType OR = new DiscreteRelType(
+		new DiscreteCalcMaxSolver(false),
+		new DiscreteCalcMinSolver(true),
+		"OR");
+
+	/**
+	 * 
+	 * @uml.property name="pLUS"
+	 * @uml.associationEnd multiplicity="(0 1)"
+	 */
+	static final DiscreteRelType PLUS = new DiscreteRelType(
+		new DiscreteCalcPartSolver(false),
+		new DiscreteCalcPTrivSolver(true),
+		"+");
+
+	/**
+	 * 
+	 * @uml.property name="pLUSPLUS"
+	 * @uml.associationEnd multiplicity="(0 1)"
+	 */
+	static final DiscreteRelType PLUSPLUS = new DiscreteRelType(
+		new DiscreteCalcCTrivSolver(false),
+		new DiscreteCalcPTrivSolver(true),
+		"++");
+
+	/**
+	 * 
+	 * @uml.property name="mINUS"
+	 * @uml.associationEnd multiplicity="(0 1)"
+	 */
+	static final DiscreteRelType MINUS = new DiscreteRelType(
+		new DiscreteCalcPTrivSolver(false),
+		new DiscreteCalcPartSolver(false),
+		"-");
+
+	/**
+	 * 
+	 * @uml.property name="mINUSMINUS"
+	 * @uml.associationEnd multiplicity="(0 1)"
+	 */
+	static final DiscreteRelType MINUSMINUS = new DiscreteRelType(
+		new DiscreteCalcPTrivSolver(false),
+		new DiscreteCalcCTrivSolver(false),
+		"--");
+
+	/**
+	 * 
+	 * @uml.property name="sTAR"
+	 * @uml.associationEnd multiplicity="(0 1)"
+	 */
+	static final DiscreteRelType STAR = new DiscreteRelType(
+		new DiscreteCalcPTrivSolver(false),
+		new DiscreteCalcPartSolver(true),
+		"*");
+
+	/**
+	 * 
+	 * @uml.property name="sTARSTAR"
+	 * @uml.associationEnd multiplicity="(0 1)"
+	 */
+	static final DiscreteRelType STARSTAR = new DiscreteRelType(
+		new DiscreteCalcPTrivSolver(false),
+		new DiscreteCalcCTrivSolver(true),
+		"**");
+
+	/**
+	 * 
+	 * @uml.property name="sLASH"
+	 * @uml.associationEnd multiplicity="(0 1)"
+	 */
+	static final DiscreteRelType SLASH = new DiscreteRelType(
+		new DiscreteCalcPartSolver(true),
+		new DiscreteCalcPTrivSolver(true),
+		"/");
+
+	/**
+	 * 
+	 * @uml.property name="sLASHSLASH"
+	 * @uml.associationEnd multiplicity="(0 1)"
+	 */
+	static final DiscreteRelType SLASHSLASH = new DiscreteRelType(
+		new DiscreteCalcCTrivSolver(true),
+		new DiscreteCalcPTrivSolver(true),
+		"//");
+
+    private DiscreteRelType(Solver aSatSolver, 
+			    Solver aDenSolver, String aName) {
+	satSolver = aSatSolver;
+	denSolver = aDenSolver;
+	myName = aName;
+	relTypePool.add(this);
+    }
+
+    String getName() {
+	return myName;
+    }
+
+    private static String getClassName() {
+	return relTypePool.getFirst().getClass().getName();
+    }
+
+    // due solver, due metodi
+
+    Label solveForS(Node parentNode, LinkedList childNodes) {
+	return satSolver.solve(parentNode, childNodes);
+    }
+
+    Label solveForD(Node parentNode, LinkedList childNodes) {
+	return denSolver.solve(parentNode, childNodes);
+    }
+
+
+
+    static RelType findRelType(String name) 
+    throws MissingResourceException {
+	for (Iterator relTypeIt = relTypePool.iterator(); 
+	     relTypeIt.hasNext();) {
+	    RelType aRelType = (RelType)relTypeIt.next();
+	    if (aRelType.getName().compareTo(name) == 0) 
+		return aRelType;
+	}
+	throw new MissingResourceException
+	    ("cannot resolve string into relation type", getClassName(), name);
+    }
+
+
+
+    public static void main(String[] args) {
+	System.out.println("Available " + getClassName());
+	for (Iterator relTypeIt = relTypePool.iterator(); 
+	     relTypeIt.hasNext();) {
+	    DiscreteRelType aRelType = (DiscreteRelType)relTypeIt.next();
+	    String s1 = aRelType.satSolver.getClass().getName();
+	    String s2 = aRelType.denSolver.getClass().getName();
+	    int x1 = s1.lastIndexOf(".") + 1;
+	    int x2 = s2.lastIndexOf(".") + 1;
+	    System.out.println("("  
+			       + s1.substring(x1)
+			       + aRelType.satSolver.getParameter()
+			       + " - "
+			       + s2.substring(x2)
+			       + aRelType.denSolver.getParameter()
+			       + ") "
+			       + aRelType.getName());
+	}
+    }
+
+}
