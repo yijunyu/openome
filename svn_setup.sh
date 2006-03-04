af5af16d030cfa52c51b632b24f0fc40ec1bbafb
yijun <yijun@1ab8f05b-f909-0410-947c-871aca028256>
2006-03-04 04:02:33 +0000

    build script
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@55 1ab8f05b-f909-0410-947c-871aca028256

--- svn_setup.sh
+++
@@ -1,7 +0,0 @@
-\rm -rf .configuration/*.*
-\rm -rf ~/repo/openome-sdk
-\rm -rf `find . -name .svn`
-svnadmin create ~/repo/openome-sdk
-svn checkout file:///h/27/yijun/repo/openome-sdk .
-svn add * > /dev/null
-svn commit -m "init"
