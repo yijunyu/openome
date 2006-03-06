5e7502c293bf1a270ec289b748197faf34033048
yijun <yijun@1ab8f05b-f909-0410-947c-871aca028256>
2006-03-06 15:36:01 +0000

    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@72 1ab8f05b-f909-0410-947c-871aca028256

--- eclipse.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set ECLIPSE_HOME=D:\IDE\sdk\3.2M4\eclipse
-D:\IDE\junction %ECLIPSE_HOME%\links .configuration\links
-copy %ECLIPSE_HOME%\configuration\config.ini .configuration
-%ECLIPSE_HOME%\eclipse.exe -vm "C:\Program files\java\jre1.5.0_06\bin\javaw.exe" -configuration .configuration -data . -showLocation -nosplash -vmargs -Xms256m -Xmx386m
-D:\IDE\junction /d %ECLIPSE_HOME%\links
