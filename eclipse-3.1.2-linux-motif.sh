a12fbb7a39ab6dd055bc53cf4d85e5c683ededbc
yijun <yijun@1ab8f05b-f909-0410-947c-871aca028256>
2006-03-07 21:54:04 +0000

    fix startup linux script
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@80 1ab8f05b-f909-0410-947c-871aca028256

--- eclipse-3.1.2-linux-motif.sh
+++ eclipse-3.1.2-linux-motif.sh
@@ -6,8 +6,8 @@ echo path=$HOME/IDE/test/3.1.2 > $ECLIPSE_HOME/links/test.link
 echo path=$HOME/IDE/subclipse/0.9.105 > $ECLIPSE_HOME/links/subclipse.link
 echo path=$HOME/IDE/mylar/0.4.6.1 > $ECLIPSE_HOME/links/mylar.link
 echo path=$HOME/IDE/openome/0.0.14 > $ECLIPSE_HOME/links/openome.link
+mkdir -p .configuration
 cp $ECLIPSE_HOME/configuration/config.ini .configuration
-export LD_LIBRARY_PATH=$ECLIPSE_HOME
-$ECLIPSE_HOME/eclipse -showLocation -configuration .configuration -data . -nosplash -vmargs -Xms256m -Xmx512m
+LD_LIBRARY_PATH=$ECLIPSE_HOME $ECLIPSE_HOME/eclipse -showLocation -configuration .configuration -data . -nosplash -vmargs -Xms256m -Xmx512m
 rm -rf $ECLIPSE_HOME/links
-rm -f .configuration/config.ini
+rm -rf .configuration
