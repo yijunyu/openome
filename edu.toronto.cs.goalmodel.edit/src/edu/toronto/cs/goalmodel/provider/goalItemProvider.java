13c1bddfe165e7f34054ba92ba797a72d4e1740d
yijun <yijun@1ab8f05b-f909-0410-947c-871aca028256>
2006-11-14 18:58:05 +0000

    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/branches/convertor@341 1ab8f05b-f909-0410-947c-871aca028256

---
+++ edu.toronto.cs.goalmodel.edit/src/edu/toronto/cs/goalmodel/provider/goalItemProvider.java
@@ -0,0 +1,488 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package edu.toronto.cs.goalmodel.provider;
+
+
+import edu.toronto.cs.goalmodel.GoalmodelFactory;
+import edu.toronto.cs.goalmodel.GoalmodelPackage;
+import edu.toronto.cs.goalmodel.goal;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
+import org.eclipse.emf.edit.provider.IItemLabelProvider;
+import org.eclipse.emf.edit.provider.IItemPropertySource;
+import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
+import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ItemProviderAdapter;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+
+/**
+ * This is the item provider adapter for a {@link edu.toronto.cs.goalmodel.goal} object.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class goalItemProvider
+	extends ItemProviderAdapter
+	implements	
+		IEditingDomainItemProvider,	
+		IStructuredItemContentProvider,	
+		ITreeItemContentProvider,	
+		IItemLabelProvider,	
+		IItemPropertySource {
+	/**
+	 * This constructs an instance from a factory and a notifier.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public goalItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	/**
+	 * This returns the property descriptors for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public List getPropertyDescriptors(Object object) {
+		if (itemPropertyDescriptors == null) {
+			super.getPropertyDescriptors(object);
+
+			addNamePropertyDescriptor(object);
+			addTypePropertyDescriptor(object);
+			addLabelPropertyDescriptor(object);
+			addSystemPropertyDescriptor(object);
+			addBoundaryPropertyDescriptor(object);
+			addExclusivePropertyDescriptor(object);
+			addSequentialPropertyDescriptor(object);
+			addParallelPropertyDescriptor(object);
+			addDependencyFromPropertyDescriptor(object);
+			addDependencyToPropertyDescriptor(object);
+			addModePropertyDescriptor(object);
+		}
+		return itemPropertyDescriptors;
+	}
+
+	/**
+	 * This adds a property descriptor for the Name feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addNamePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_goal_name_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_goal_name_feature", "_UI_goal_type"),
+				 GoalmodelPackage.Literals.GOAL__NAME,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Type feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addTypePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_goal_type_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_goal_type_feature", "_UI_goal_type"),
+				 GoalmodelPackage.Literals.GOAL__TYPE,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Label feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addLabelPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_goal_label_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_goal_label_feature", "_UI_goal_type"),
+				 GoalmodelPackage.Literals.GOAL__LABEL,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the System feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addSystemPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_goal_system_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_goal_system_feature", "_UI_goal_type"),
+				 GoalmodelPackage.Literals.GOAL__SYSTEM,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Boundary feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addBoundaryPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_goal_boundary_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_goal_boundary_feature", "_UI_goal_type"),
+				 GoalmodelPackage.Literals.GOAL__BOUNDARY,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Exclusive feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addExclusivePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_goal_exclusive_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_goal_exclusive_feature", "_UI_goal_type"),
+				 GoalmodelPackage.Literals.GOAL__EXCLUSIVE,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Sequential feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addSequentialPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_goal_sequential_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_goal_sequential_feature", "_UI_goal_type"),
+				 GoalmodelPackage.Literals.GOAL__SEQUENTIAL,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Parallel feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addParallelPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_goal_parallel_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_goal_parallel_feature", "_UI_goal_type"),
+				 GoalmodelPackage.Literals.GOAL__PARALLEL,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Dependency From feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addDependencyFromPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_goal_dependencyFrom_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_goal_dependencyFrom_feature", "_UI_goal_type"),
+				 GoalmodelPackage.Literals.GOAL__DEPENDENCY_FROM,
+				 true,
+				 false,
+				 false,
+				 null,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Dependency To feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addDependencyToPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_goal_dependencyTo_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_goal_dependencyTo_feature", "_UI_goal_type"),
+				 GoalmodelPackage.Literals.GOAL__DEPENDENCY_TO,
+				 true,
+				 false,
+				 false,
+				 null,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Mode feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addModePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_goal_mode_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_goal_mode_feature", "_UI_goal_type"),
+				 GoalmodelPackage.Literals.GOAL__MODE,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Collection getChildrenFeatures(Object object) {
+		if (childrenFeatures == null) {
+			super.getChildrenFeatures(object);
+			childrenFeatures.add(GoalmodelPackage.Literals.GOAL__GOAL);
+			childrenFeatures.add(GoalmodelPackage.Literals.GOAL__RULE);
+			childrenFeatures.add(GoalmodelPackage.Literals.GOAL__INPUT);
+			childrenFeatures.add(GoalmodelPackage.Literals.GOAL__OUTPUT);
+			childrenFeatures.add(GoalmodelPackage.Literals.GOAL__PROPERTY);
+			childrenFeatures.add(GoalmodelPackage.Literals.GOAL__ANNOTATION);
+		}
+		return childrenFeatures;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected EStructuralFeature getChildFeature(Object object, Object child) {
+		// Check the type of the specified child object and return the proper feature to use for
+		// adding (see {@link AddCommand}) it as a child.
+
+		return super.getChildFeature(object, child);
+	}
+
+	/**
+	 * This returns goal.gif.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Object getImage(Object object) {
+		return overlayImage(object, getResourceLocator().getImage("full/obj16/goal"));
+	}
+
+	/**
+	 * This returns the label text for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getText(Object object) {
+		String label = ((goal)object).getName();
+		return label == null || label.length() == 0 ?
+			getString("_UI_goal_type") :
+			getString("_UI_goal_type") + " " + label;
+	}
+
+	/**
+	 * This handles model notifications by calling {@link #updateChildren} to update any cached
+	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void notifyChanged(Notification notification) {
+		updateChildren(notification);
+
+		switch (notification.getFeatureID(goal.class)) {
+			case GoalmodelPackage.GOAL__NAME:
+			case GoalmodelPackage.GOAL__TYPE:
+			case GoalmodelPackage.GOAL__LABEL:
+			case GoalmodelPackage.GOAL__SYSTEM:
+			case GoalmodelPackage.GOAL__BOUNDARY:
+			case GoalmodelPackage.GOAL__EXCLUSIVE:
+			case GoalmodelPackage.GOAL__SEQUENTIAL:
+			case GoalmodelPackage.GOAL__PARALLEL:
+			case GoalmodelPackage.GOAL__MODE:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+				return;
+			case GoalmodelPackage.GOAL__GOAL:
+			case GoalmodelPackage.GOAL__RULE:
+			case GoalmodelPackage.GOAL__INPUT:
+			case GoalmodelPackage.GOAL__OUTPUT:
+			case GoalmodelPackage.GOAL__PROPERTY:
+			case GoalmodelPackage.GOAL__ANNOTATION:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+				return;
+		}
+		super.notifyChanged(notification);
+	}
+
+	/**
+	 * This adds to the collection of {@link org.eclipse.emf.edit.command.CommandParameter}s
+	 * describing all of the children that can be created under this object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void collectNewChildDescriptors(Collection newChildDescriptors, Object object) {
+		super.collectNewChildDescriptors(newChildDescriptors, object);
+
+		newChildDescriptors.add
+			(createChildParameter
+				(GoalmodelPackage.Literals.GOAL__GOAL,
+				 GoalmodelFactory.eINSTANCE.creategoal()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(GoalmodelPackage.Literals.GOAL__RULE,
+				 GoalmodelFactory.eINSTANCE.createcontribution()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(GoalmodelPackage.Literals.GOAL__INPUT,
+				 GoalmodelFactory.eINSTANCE.createtopic()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(GoalmodelPackage.Literals.GOAL__OUTPUT,
+				 GoalmodelFactory.eINSTANCE.createtopic()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(GoalmodelPackage.Literals.GOAL__PROPERTY,
+				 GoalmodelFactory.eINSTANCE.createproperty()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(GoalmodelPackage.Literals.GOAL__ANNOTATION,
+				 GoalmodelFactory.eINSTANCE.createlinkAnnotation()));
+	}
+
+	/**
+	 * This returns the label text for {@link org.eclipse.emf.edit.command.CreateChildCommand}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getCreateChildText(Object owner, Object feature, Object child, Collection selection) {
+		Object childFeature = feature;
+		Object childObject = child;
+
+		boolean qualify =
+			childFeature == GoalmodelPackage.Literals.GOAL__INPUT ||
+			childFeature == GoalmodelPackage.Literals.GOAL__OUTPUT;
+
+		if (qualify) {
+			return getString
+				("_UI_CreateChild_text2",
+				 new Object[] { getTypeText(childObject), getFeatureText(childFeature), getTypeText(owner) });
+		}
+		return super.getCreateChildText(owner, feature, child, selection);
+	}
+
+	/**
+	 * Return the resource locator for this item provider's resources.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ResourceLocator getResourceLocator() {
+		return GoalEditPlugin.INSTANCE;
+	}
+
+}
