639b888588a3649fa9ceeb791b92232bca0556fd
yijun <yijun@1ab8f05b-f909-0410-947c-871aca028256>
2006-11-15 16:23:02 +0000

    1) refactored the startup batch file 2) resolved a cyclic dependency by the duplicate version of emf (the EMF feature from the downloaded BPEL 0.0.1 feature    is removed) 3) updated the build.properties of several plugins to include the manifests files
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/branches/convertor@347 1ab8f05b-f909-0410-947c-871aca028256

--- eclipse-3.2-win32.bat
+++ eclipse-3.2-win32.bat
@@ -1,30 +1,79 @@
 @echo off
-REM -- Change the following paths according to your file system --
-set IDE=C:\IDE
-set IDE2=C:/IDE
 set JVM="C:\Program files\java\jre1.5.0_06\bin\javaw.exe"
-REM --------------------------------------------------------------
-set ECLIPSE_HOME=%IDE%\sdk\3.2\eclipse
+goto :main
+
+:configure_eclipse_products
+call :telos
+call :q7
+call :goalmodel
+call :ast
+call :fmp
+call :bpel
+goto :eof
+
+:main
+call :get_ide_path
+call :configure_eclipse_path
+call :configure_eclipse_products
+call :start_eclipse
+goto :eof
+
+:configure_eclipse_path
+set ECLIPSE_HOME=%CD%\IDE\sdk\3.2\eclipse
 rmdir /q /s %ECLIPSE_HOME%\links
 mkdir %ECLIPSE_HOME%\links
-REM For user
-echo path=%IDE2%/emf/2.2.0 > %ECLIPSE_HOME%\links\emf.link
-echo path=%IDE2%/test/3.2.0 > %ECLIPSE_HOME%\links\test.link
-echo path=%IDE2%/batik/1.6.0 > %ECLIPSE_HOME%\links\batik.link
-echo path=%IDE2%/emft/1.0.1 > %ECLIPSE_HOME%\links\emft.link
-echo path=%IDE2%/xerces/2.8.0 > %ECLIPSE_HOME%\links\xerces.link
-echo path=%IDE2%/bpel/0.0.1 > %ECLIPSE_HOME%\links\bpel.link
-echo path=%IDE2%/fmp/0.6.6 > %ECLIPSE_HOME%\links\fmp.link
-echo path=%IDE2%/delta/3.2.0 > %ECLIPSE_HOME%\links\delta.link
-REM For development
-REM echo path=%IDE2%/openome/0.0.17 > %ECLIPSE_HOME%\links\openome.link
-REM echo path=%IDE2%/subclipse/1.0.3 > %ECLIPSE_HOME%\links\subclipse.link
-REM echo path=%IDE2%/mylar/0.6.6 > %ECLIPSE_HOME%\links\mylar.link
-REM echo path=%IDE2%/google/1.0.0 > %ECLIPSE_HOME%\links\google.link
-echo path=%IDE2%/pure/2.2.5 > %ECLIPSE_HOME%\links\pure.link
 set DATA=.
 set CONFIG=%DATA%\.configuration
 rmdir /q /s %CONFIG%
 mkdir %CONFIG%
 copy %ECLIPSE_HOME%\configuration\config.ini %CONFIG%
-%ECLIPSE_HOME%\eclipse.exe -vm %JVM% -configuration %CONFIG% -data %DATA% -showLocation -nosplash -vmargs -Xms256m -Xmx512m
+goto :eof
+
+:start_eclipse
+start /b %ECLIPSE_HOME%\eclipse.exe -vm %JVM% -configuration %CONFIG% -data %DATA% -showLocation -nosplash -vmargs -Xms256m -Xmx512m
+goto :eof
+
+:q7
+goto :eof
+
+:telos
+call :link_product batik 1.6.0
+goto :eof
+
+:ast
+call :link_product emf 2.2.0
+goto :eof
+
+:goalmodel
+call :link_product emf 2.2.0
+goto :eof
+
+:fmp
+call :link_product emf 2.2.0
+call :link_product fmp 0.6.6
+goto :eof
+
+:bpel
+call :link_product emf 2.2.0
+call :link_product jem 1.2.0
+call :link_product draw2d 2.2.0
+call :link_product gef 3.2.0M2
+call :link_product xerces 2.8.0
+call :link_product wsdl4j 1.4.0.v200606181221
+call :link_product wst 1.0.1
+call :link_product bpel 0.0.1
+goto :eof
+
+:get_ide_path
+echo %CD%\IDE | IDE\sed "s/\\/\//g" | IDE\sed "s/ //" > t.t
+set /p ide_path=< t.t
+del t.t
+goto :eof
+
+:link_product
+setlocal
+REM %1 = product name
+REM %2 = product version
+echo path=%ide_path%/%1/%2 > %ECLIPSE_HOME%\links\%1.link
+endlocal
+goto :eof
