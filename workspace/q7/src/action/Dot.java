e78f23427b24099a50a8ecedebc18fc372c4e201
nernst <nernst@1ab8f05b-f909-0410-947c-871aca028256>
2007-08-31 22:26:29 +0000

    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@524 1ab8f05b-f909-0410-947c-871aca028256

--- workspace/q7/src/action/Dot.java
+++
@@ -1,8 +0,0 @@
-package action;
-
-
-public class Dot extends ConvertAction {
-	public Dot() {
-		super(".q7", ".dot");
-	}	
-}
--- workspace/q7/src/action/EMF.java
+++
@@ -1,8 +0,0 @@
-package action;
-
-
-public class EMF extends ConvertAction {
-	public EMF() {
-		super(".q7", ".q7.dsl");
-	}	
-}
