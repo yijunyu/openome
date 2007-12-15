3063d8177bd4876b8c51ce498e0ea6fb36ecd5e9
yijun <yijun@1ab8f05b-f909-0410-947c-871aca028256>
2007-12-15 00:03:17 +0000

    update
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@643 1ab8f05b-f909-0410-947c-871aca028256

---
+++ episteme/runtime.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export ECLIPSE_SDK=/IDE/sdk/3.3.1.1
+rm -rf configuration
+mkdir configuration
+cp $ECLIPSE_SDK/eclipse/configuration/config.ini configuration
+rm -rf $ECLIPSE_SDK/eclipse/links
+mkdir $ECLIPSE_SDK/eclipse/links
+cp linux/*.* $ECLIPSE_SDK/eclipse/links
+$ECLIPSE_SDK/eclipse/eclipse -plugincustomization plugin_customization.ini -configuration configuration -data runtime-$1
