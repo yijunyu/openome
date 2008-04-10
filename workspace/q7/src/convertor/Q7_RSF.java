9d8b60f3d58c1428e5d7a7d7e3270f756d290dce
nernst <nernst@1ab8f05b-f909-0410-947c-871aca028256>
2008-04-10 16:49:11 +0000

    copying GMF2 projects to trunk from neil/workspace-gmf2
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@832 1ab8f05b-f909-0410-947c-871aca028256

---
+++ workspace/q7/src/convertor/Q7_RSF.java
@@ -0,0 +1,17 @@
+package convertor;
+
+import parser.Q7;
+import codegen.RSF;
+
+public class Q7_RSF implements IConvertor {
+	public void convert(String input, String output) {
+		Q7.main(new String[] {input});	
+		RSF cg = new RSF(Q7.a);
+		cg.generateRSF(output);		
+	}
+	
+	public static void main(String[] args) {
+		Q7_RSF c = new Q7_RSF();
+		c.convert(args[0], args[1]);
+	}
+}
