f6fb13b597818e207b53be029c13ac95fc796716
yijun <yijun@1ab8f05b-f909-0410-947c-871aca028256>
2008-05-07 18:20:54 +0000

    create a new trunk
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@853 1ab8f05b-f909-0410-947c-871aca028256

--- workspace/openome_model/src/action/Q7_GoalModel.java
+++
@@ -1,7 +0,0 @@
-package action;
-
-public class Q7_GoalModel extends ConvertAction {
-	public Q7_GoalModel() {
-		super(".q7", ".oom");
-	}
-}
--- workspace/telos/src/edu/toronto/cs/ome/view/ViewContext.java
+++
@@ -1,63 +0,0 @@
-package edu.toronto.cs.ome.view;
-
-import java.util.Collection;
-
-/**
- * This class is convey certain information about the state of a particular  * view to a PluginMethod when the latter is called. At the moment the * information conveyed includes *  * <li>the objects within the view that are selected  * <li>the location of the mouse-click that caused the <PluginMethod> to be  * called. * <li>an object in the view to be associated with this call to the * PluginMethod.
- */
-
-
-public class ViewContext {
-
-	/**
-	 * 
-	 * @uml.property name="l"
-	 * @uml.associationEnd multiplicity="(0 1)"
-	 */
-	private View.Location l;
-
-	/**
-	 * 
-	 * @uml.property name="o"
-	 * @uml.associationEnd multiplicity="(0 1)"
-	 */
-	private ViewObject o;
-
-    private Collection c;
-
-    /** Creates a ViewContext. 
-      *
-      * @param l the location of the mouse-click that caused the <PluginMethod>
-      * to be called.
-      * @param associatedobject an object in the view to be associated with 
-      * this call to the PluginMethod.
-      * @param selectedobjects the objects within the view that are selected 
-      */
-    ViewContext(View.Location l, ViewObject associatedobject, Collection
-	    selectedobjects) {
-
-	this.l = l;
-	this.o = associatedobject;
-	this.c = selectedobjects;
-    }
-
-    /** Returns the View.Location associated with the initiation of the call
-     *  to the method (typically a mouse click).  This method returns null 
-     *  if no Location can be associated with the event.
-     */
-    public View.Location associatedLocation() {
-	return l;
-    }
-
-    /** Returns the ViewObject that is at the associatedLocation (if any). */
-    public ViewObject associatedObject() {
-	return o;
-    }
-
-    /** Returns a Collection of ViewObjects that were selected when the method
-     *  was intiated.
-     */
-    public Collection selectedObjects() {
-	return c;
-    }
-}
