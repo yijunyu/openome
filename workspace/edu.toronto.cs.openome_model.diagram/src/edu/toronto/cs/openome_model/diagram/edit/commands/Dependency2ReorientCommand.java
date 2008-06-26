a0b92700431e1115976d4ca464b07fb25d4b528a
nernst <nernst@1ab8f05b-f909-0410-947c-871aca028256>
2008-06-26 17:23:14 +0000

    adding newly generated files
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@956 1ab8f05b-f909-0410-947c-871aca028256

---
+++ workspace/edu.toronto.cs.openome_model.diagram/src/edu/toronto/cs/openome_model/diagram/edit/commands/Dependency2ReorientCommand.java
@@ -0,0 +1,165 @@
+package edu.toronto.cs.openome_model.diagram.edit.commands;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.emf.type.core.commands.EditElementCommand;
+import org.eclipse.gmf.runtime.emf.type.core.requests.ReorientRelationshipRequest;
+
+/**
+ * @generated
+ */
+public class Dependency2ReorientCommand extends EditElementCommand {
+
+	/**
+	 * @generated
+	 */
+	private final int reorientDirection;
+
+	/**
+	 * @generated
+	 */
+	private final EObject oldEnd;
+
+	/**
+	 * @generated
+	 */
+	private final EObject newEnd;
+
+	/**
+	 * @generated
+	 */
+	public Dependency2ReorientCommand(ReorientRelationshipRequest request) {
+		super(request.getLabel(), request.getRelationship(), request);
+		reorientDirection = request.getDirection();
+		oldEnd = request.getOldRelationshipEnd();
+		newEnd = request.getNewRelationshipEnd();
+	}
+
+	/**
+	 * @generated
+	 */
+	public boolean canExecute() {
+		if (!(getElementToEdit() instanceof edu.toronto.cs.openome_model.Dependency)) {
+			return false;
+		}
+		if (reorientDirection == ReorientRelationshipRequest.REORIENT_SOURCE) {
+			return canReorientSource();
+		}
+		if (reorientDirection == ReorientRelationshipRequest.REORIENT_TARGET) {
+			return canReorientTarget();
+		}
+		return false;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected boolean canReorientSource() {
+		if (!(oldEnd instanceof edu.toronto.cs.openome_model.Actor && newEnd instanceof edu.toronto.cs.openome_model.Actor)) {
+			return false;
+		}
+		if (getLink().getActorDependencyFrom().size() != 1) {
+			return false;
+		}
+		edu.toronto.cs.openome_model.Actor target = (edu.toronto.cs.openome_model.Actor) getLink()
+				.getActorDependencyFrom().get(0);
+		if (!(getLink().eContainer() instanceof edu.toronto.cs.openome_model.Model)) {
+			return false;
+		}
+		edu.toronto.cs.openome_model.Model container = (edu.toronto.cs.openome_model.Model) getLink()
+				.eContainer();
+		return edu.toronto.cs.openome_model.diagram.edit.policies.Openome_modelBaseItemSemanticEditPolicy.LinkConstraints
+				.canExistDependency_3002(container, getNewSource(), target);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected boolean canReorientTarget() {
+		if (!(oldEnd instanceof edu.toronto.cs.openome_model.Actor && newEnd instanceof edu.toronto.cs.openome_model.Actor)) {
+			return false;
+		}
+		edu.toronto.cs.openome_model.Actor source = getLink()
+				.getActorDependencyTo();
+		if (!(getLink().eContainer() instanceof edu.toronto.cs.openome_model.Model)) {
+			return false;
+		}
+		edu.toronto.cs.openome_model.Model container = (edu.toronto.cs.openome_model.Model) getLink()
+				.eContainer();
+		return edu.toronto.cs.openome_model.diagram.edit.policies.Openome_modelBaseItemSemanticEditPolicy.LinkConstraints
+				.canExistDependency_3002(container, source, getNewTarget());
+	}
+
+	/**
+	 * @generated
+	 */
+	protected CommandResult doExecuteWithResult(IProgressMonitor monitor,
+			IAdaptable info) throws ExecutionException {
+		if (!canExecute()) {
+			throw new ExecutionException(
+					"Invalid arguments in reorient link command"); //$NON-NLS-1$
+		}
+		if (reorientDirection == ReorientRelationshipRequest.REORIENT_SOURCE) {
+			return reorientSource();
+		}
+		if (reorientDirection == ReorientRelationshipRequest.REORIENT_TARGET) {
+			return reorientTarget();
+		}
+		throw new IllegalStateException();
+	}
+
+	/**
+	 * @generated
+	 */
+	protected CommandResult reorientSource() throws ExecutionException {
+		getLink().setActorDependencyTo(getNewSource());
+		return CommandResult.newOKCommandResult(getLink());
+	}
+
+	/**
+	 * @generated
+	 */
+	protected CommandResult reorientTarget() throws ExecutionException {
+		getLink().getActorDependencyFrom().remove(getOldTarget());
+		getLink().getActorDependencyFrom().add(getNewTarget());
+		return CommandResult.newOKCommandResult(getLink());
+	}
+
+	/**
+	 * @generated
+	 */
+	protected edu.toronto.cs.openome_model.Dependency getLink() {
+		return (edu.toronto.cs.openome_model.Dependency) getElementToEdit();
+	}
+
+	/**
+	 * @generated
+	 */
+	protected edu.toronto.cs.openome_model.Actor getOldSource() {
+		return (edu.toronto.cs.openome_model.Actor) oldEnd;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected edu.toronto.cs.openome_model.Actor getNewSource() {
+		return (edu.toronto.cs.openome_model.Actor) newEnd;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected edu.toronto.cs.openome_model.Actor getOldTarget() {
+		return (edu.toronto.cs.openome_model.Actor) oldEnd;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected edu.toronto.cs.openome_model.Actor getNewTarget() {
+		return (edu.toronto.cs.openome_model.Actor) newEnd;
+	}
+}
---
+++ workspace/edu.toronto.cs.openome_model.diagram/src/edu/toronto/cs/openome_model/diagram/edit/helpers/ContributionEditHelper.java
@@ -0,0 +1,9 @@
+package edu.toronto.cs.openome_model.diagram.edit.helpers;
+
+/**
+ * @generated
+ */
+public class ContributionEditHelper
+		extends
+		edu.toronto.cs.openome_model.diagram.edit.helpers.Openome_modelBaseEditHelper {
+}
