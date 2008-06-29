0e11b285584047da3b8d32817398d7bb4d7d94c3
kelvin <kelvin@1ab8f05b-f909-0410-947c-871aca028256>
2008-06-29 14:12:33 +0000

    Ticket #101 - appearance of dependency links --------------------------------------------- -this ticket is now resolved -although this is just a few lines of code changes and additions, it required a lot of blood, sweat, tears - (time and trial&error) to figure out -the link cannot curve smoothly anymore, because DependencyEditPart.java's DependencyFigure class extends our custom class DependencyLineConnection (which extends PolylineConnection - doesn't have setSmoothness method) rather than PolylineConnectionEx (which does have the setSmoothness method)
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@968 1ab8f05b-f909-0410-947c-871aca028256

---
+++ workspace/openome_model.figures/src/openome_model/figures/DependencyLineConnection.java
@@ -0,0 +1,165 @@
+package openome_model.figures;
+
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.Graphics;
+import org.eclipse.draw2d.PolylineConnection;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.PointList;
+import org.eclipse.draw2d.geometry.Rectangle;
+
+public class DependencyLineConnection extends PolylineConnection {
+	
+    private static final int stepLine = 6;
+    private static final int stepLineMin = 4;
+    
+    // the size of the 'D' decoration
+    // that will be located at the midpoint
+    private static int sizeOfD = 15;
+	
+	protected void outlineShape(Graphics g) {
+		
+		// ensure that there are no decorations on the ends
+		this.setSourceDecoration(null);
+		this.setTargetDecoration(null);
+		
+		// draw the normal connecting link/connector first
+		super.outlineShape(g);
+		
+		// determine the points of the two ends - the source and target
+		Point sourcePoint = this.getPoints().getFirstPoint();
+		Point targetPoint = this.getPoints().getLastPoint();
+		
+		int angle = (int)(calcAngle(targetPoint,sourcePoint));
+		angle = angle - 90;
+		angle = angle * -1;
+		double lineAngle = angle+90;
+		
+		// next, get the midpoint of the link/connection
+		PointList pl = this.getPoints();
+		Point midPoint = pl.getMidpoint();
+
+		// ===============================================
+		// PLAN A: load and draw an image at the midpoint
+		// ===============================================
+		
+		//Image i = new Image(null, "/Users/db/Pictures/db/revolution-db2.bmp");
+		//g.drawImage(i, midPoint);
+		
+		// -------------------
+		// rotate accordingly..
+		// -------------------
+		
+		// attempt to rotate.. this causes a very strange yet serious 
+		// crash at the moment.. it's not a regular Java exception.. 
+		// it's a bug that's currently being tracked by Eclipse right now. 
+				
+		//g.rotate(45);
+	
+		
+		// ===========================================
+		// PLAN B: draw the D using an arc and a line
+		// ===========================================
+		
+		// ------------
+		// draw the arc
+		// ------------
+		
+		g.drawArc(midPoint.x-sizeOfD, midPoint.y-sizeOfD, sizeOfD*2, sizeOfD*2, angle, 180);
+		
+		// fill in the arc so that you don't see the link/connector line
+		g.fillArc(midPoint.x-sizeOfD, midPoint.y-sizeOfD, sizeOfD*2, sizeOfD*2, angle, 180);
+		
+		
+		// --------------
+		// draw the line
+		// --------------
+		
+		// convert the rotation angle from degrees to radians
+		double line_Angle_Radians = this.degrees2Radians(lineAngle);
+
+		Point linePoint_One = new Point((int)(midPoint.x+(Math.sin(line_Angle_Radians)*sizeOfD)), 
+										(int)(midPoint.y+(Math.cos(line_Angle_Radians)*sizeOfD)));
+		
+		Point linePoint_Two = new Point((int)(midPoint.x-(Math.sin(line_Angle_Radians)*sizeOfD)), 
+										(int)(midPoint.y-(Math.cos(line_Angle_Radians)*sizeOfD)));
+		
+		// render the line
+		g.drawLine(linePoint_One, linePoint_Two);
+
+	}
+	
+	public Rectangle getBounds() {
+		Rectangle rect = super.getBounds();
+		rect.union(rect.x - 3 * stepLine, rect.y - 3 * stepLine);
+		rect.union(rect.x + rect.width + 3 * stepLine, rect.y + rect.height + 3
+				* stepLine);
+		return rect;
+	}
+	
+	/**
+	 * Calculate the angle (in degrees) between two points.
+	 * Reference: http://www.uk-dave.com/bytes/java/angle_between_points.html
+	 * @param p the first point
+	 * @param q the second point
+	 * @return the angle (in degrees) between point p and point q
+	 */
+	public static double calcAngle(Point p, Point q)
+    {
+		
+		float x1 = p.x;
+		float y1 = p.y;
+		float x2 = q.x;
+		float y2 = q.y;
+		
+        float dx = x2-x1;
+        float dy = y2-y1;
+        double angle=0.0d;
+ 
+        // Calculate angle
+        if (dx == 0.0)
+        {
+            if (dy == 0.0)
+                angle = 0.0;
+            else if (dy > 0.0)
+                angle = Math.PI / 2.0;
+            else
+                angle = Math.PI * 3.0 / 2.0;
+        }
+        else if (dy == 0.0)
+        {
+            if  (dx > 0.0)
+                angle = 0.0;
+            else
+                angle = Math.PI;
+        }
+        else
+        {
+            if  (dx < 0.0)
+                angle = Math.atan(dy/dx) + Math.PI;
+            else if (dy < 0.0)
+                angle = Math.atan(dy/dx) + (2*Math.PI);
+            else
+                angle = Math.atan(dy/dx);
+        }
+ 
+        // Convert to degrees
+        angle = angle * 180 / Math.PI;
+ 
+        // Return
+        return angle;
+    }
+	
+	/**
+	 * Convert an angle from degrees to radians.
+	 * @param degrees the angle in degrees
+	 * @return the angle in radians
+	 */
+	public static double degrees2Radians (double degrees)
+    {
+      double radians = (Math.PI / 180) * degrees;
+      return (radians);
+    }  
+
+
+	
+}
