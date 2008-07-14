9731971e880ff39343aeb4398048b20061696336
kelvin <kelvin@1ab8f05b-f909-0410-947c-871aca028256>
2008-07-14 21:29:14 +0000

    -This is for Ticket #68, and for Ticket #67 -this change made it so that the softgoal figure is black (use original colours) rather than grey.. -this is done by passing in a 'true' to the ScalableImageFigure constructor for using original colours -also, for ticket #67, we now call setMaintainAspectRatio(true), which works (kind of) - the figure does indeed maintain it's aspect ratio, but does not consider the resize border.... please see ticket #67 for an attachment, buggy.png -a 'user' fix is possible - when resizing, hold down the shift key.. but i'm looking into having this done permanently, which is indeed possible.. there is another GMF RCP called PARTE (http://sourceforge.net/projects/parte) that can do this
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@1030 1ab8f05b-f909-0410-947c-871aca028256

--- workspace/openome_model.figures/src/openome_model/figures/SoftGoalSVGFigure.java
+++ workspace/openome_model.figures/src/openome_model/figures/SoftGoalSVGFigure.java
@@ -1,17 +1,29 @@
 package openome_model.figures;
 
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.StackLayout;
+import org.eclipse.gmf.runtime.draw2d.ui.render.factory.RenderedImageFactory;
+import org.eclipse.gmf.runtime.draw2d.ui.render.figures.ScalableImageFigure;
 
-public class SoftGoalSVGFigure extends NodeSVGFigure {
+public class SoftGoalSVGFigure extends ScalableImageFigure {
 
 	public SoftGoalSVGFigure() {
-		super("softgoal");
+		super(RenderedImageFactory.getInstance(FileLocator.find(Platform.getBundle("openome_model"), new Path("images/" + "softgoal" + ".svg"), null)), 
+				true, true, true);
+		setMaintainAspectRatio(true);
+		
 		setLayoutManager(new StackLayout());
 		setBackgroundColor(ColorConstants.red);
 	}
 	public SoftGoalSVGFigure(String name) {
-		super("softgoal");
+		super(RenderedImageFactory.getInstance(FileLocator.find(Platform.getBundle("openome_model"), new Path("images/" + name + ".svg"), null)), 
+				true, true, true);
+		setMaintainAspectRatio(true);
+		
+		
 		setLayoutManager(new StackLayout());
 		setBackgroundColor(ColorConstants.red);
 	}
