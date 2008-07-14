c1c044c96996cbb4e26dfe09c6cc2e1e08681466
kelvin <kelvin@1ab8f05b-f909-0410-947c-871aca028256>
2008-07-14 15:24:47 +0000

    -smooth curvature for new contribution links
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@1025 1ab8f05b-f909-0410-947c-871aca028256

--- workspace/edu.toronto.cs.openome_model.diagram/src/edu/toronto/cs/openome_model/diagram/view/factories/SomeMinusContributionViewFactory.java
+++ workspace/edu.toronto.cs.openome_model.diagram/src/edu/toronto/cs/openome_model/diagram/view/factories/SomeMinusContributionViewFactory.java
@@ -9,6 +9,8 @@ import org.eclipse.gmf.runtime.diagram.core.util.ViewUtil;
 import org.eclipse.gmf.runtime.diagram.ui.view.factories.ConnectionViewFactory;
 import org.eclipse.gmf.runtime.emf.core.util.EObjectAdapter;
 import org.eclipse.gmf.runtime.notation.NotationFactory;
+import org.eclipse.gmf.runtime.notation.NotationPackage;
+import org.eclipse.gmf.runtime.notation.RoutingStyle;
 import org.eclipse.gmf.runtime.notation.View;
 
 /**
@@ -51,5 +53,12 @@ public class SomeMinusContributionViewFactory extends ConnectionViewFactory {
 						edu.toronto.cs.openome_model.diagram.part.Openome_modelVisualIDRegistry
 								.getType(edu.toronto.cs.openome_model.diagram.edit.parts.SomeMinusContributionContributionTypeEditPart.VISUAL_ID),
 						ViewUtil.APPEND, true, getPreferencesHint());
+		
+		// ensures that the link/connectors have smooth curvature
+		RoutingStyle style = (RoutingStyle) view
+				.getStyle(NotationPackage.eINSTANCE.getRoutingStyle());
+		style
+				.setSmoothness(org.eclipse.gmf.runtime.notation.Smoothness.NORMAL_LITERAL);
+		
 	}
 }
