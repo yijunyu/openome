9e0216ffb8243383e64cc6c4a41b6691b7cb322d
nernst <nernst@1ab8f05b-f909-0410-947c-871aca028256>
2008-09-02 19:00:43 +0000

    mistake
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@1160 1ab8f05b-f909-0410-947c-871aca028256

--- workspace/workspace/edu.toronto.cs.openome.conversion/src/edu/toronto/cs/openome/conversion/service/IConfigurator.java
+++
@@ -1,6 +0,0 @@
-package edu.toronto.cs.openome.conversion.service;
-
-public interface IConfigurator {
-	public void setModel(String name);
-	public String query(String name);
-}
--- workspace/workspace/it.unitn.goal_analysis/src/it/unitn/goal_analysis/graph_creation/RelType.java
+++
@@ -1,9 +0,0 @@
-package it.unitn.goal_analysis.graph_creation; 
-import java.util.LinkedList;
-
-
-public abstract class RelType {
-    abstract Label solveForS(Node parentNode, LinkedList childNodes); 
-    abstract Label solveForD(Node parentNode, LinkedList childNodes); 
-    abstract String getName();             // returns the type of the relation
-}
