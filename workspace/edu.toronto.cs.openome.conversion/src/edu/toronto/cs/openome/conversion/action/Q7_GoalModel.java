5cdaeac03ffb2a20cc2a8bfdcf1cf98e166633ba
nernst <nernst@1ab8f05b-f909-0410-947c-871aca028256>
2008-09-02 19:01:56 +0000

    restore
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@1161 1ab8f05b-f909-0410-947c-871aca028256

---
+++ workspace/edu.toronto.cs.openome.conversion/src/edu/toronto/cs/openome/conversion/action/Q7_GoalModel.java
@@ -0,0 +1,9 @@
+package edu.toronto.cs.openome.conversion.action;
+
+import edu.toronto.cs.openome.core.action.ConvertAction;
+
+public class Q7_GoalModel extends ConvertAction {
+	public Q7_GoalModel() {
+		super(".q7", ".oom");
+	}
+}
