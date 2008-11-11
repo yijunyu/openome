06b128585b2ef8ae74fbfffacb9f0601c2a8c49b
kelvin <kelvin@1ab8f05b-f909-0410-947c-871aca028256>
2008-11-11 21:39:06 +0000

    -resolved ticket #144 !! The ability to apply evaluation labels quickly and easily via the context menu (unfortunately, not via the coolbar)
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@1201 1ab8f05b-f909-0410-947c-871aca028256

---
+++ workspace/edu.toronto.cs.openome_model.diagram/src/customsrc/OpenOMEActionProvider.java
@@ -0,0 +1,36 @@
+package customsrc;
+
+import org.eclipse.gmf.runtime.common.ui.services.action.contributionitem.AbstractContributionItemProvider;
+import org.eclipse.gmf.runtime.common.ui.util.IWorkbenchPartDescriptor;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.ui.IWorkbenchPage;
+
+
+public class OpenOMEActionProvider extends AbstractContributionItemProvider{
+
+	
+	protected IAction createAction(String actionId,
+            IWorkbenchPartDescriptor partDescriptor) {
+
+        IWorkbenchPage workbenchPage = partDescriptor.getPartPage();
+        
+        if (actionId.equals("SetNoneLabelAction")) {
+        	return new SetNoneLabelAction(workbenchPage);
+        } else if (actionId.equals("SetSatisfiedLabelAction")) {
+        	return new SetSatisfiedLabelAction(workbenchPage);
+        } else if (actionId.equals("SetWeaklySatisfiedLabelAction")) {
+        	return new SetWeaklySatisfiedLabelAction(workbenchPage);
+        } else if (actionId.equals("SetConflictLabelAction")) {
+        	return new SetConflictLabelAction(workbenchPage);
+        } else if (actionId.equals("SetWeaklyDeniedLabelAction")) {
+        	return new SetWeaklyDeniedLabelAction(workbenchPage);
+        } else if (actionId.equals("SetDeniedLabelAction")) {
+        	return new SetDeniedLabelAction(workbenchPage);
+        } else if (actionId.equals("SetUnknownLabelAction")) {
+        	return new SetUnknownLabelAction(workbenchPage);
+        } else {
+        	return null;        	
+        }
+	}
+}
