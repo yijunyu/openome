c2acb204e947a037cec8ca3e8ae6c247cebf40c4
jenhork <jenhork@1ab8f05b-f909-0410-947c-871aca028256>
2009-07-15 21:53:19 +0000

    Evaluation Changes
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@1225 1ab8f05b-f909-0410-947c-871aca028256

---
+++ workspace/edu.toronto.cs.openome.evaluation/src/edu/toronto/cs/openome/evaluation/commands/SetQualitativeEvaluationLabelCommand.java
@@ -0,0 +1,86 @@
+package edu.toronto.cs.openome.evaluation.commands;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.command.Command;
+
+import edu.toronto.cs.openome_model.EvaluationLabel;
+import edu.toronto.cs.openome_model.Intention;
+
+public class SetQualitativeEvaluationLabelCommand implements Command {
+	private Intention intention;
+	
+	public SetQualitativeEvaluationLabelCommand(Intention i) {
+		intention = i;
+	}
+
+	@Override
+	public boolean canExecute() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean canUndo() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Command chain(Command arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void execute() {
+		System.out.println("Execute");
+		
+		intention.setQualitativeReasoningCombinedLabel(EvaluationLabel.SATISFIED);
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Collection<?> getAffectedObjects() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLabel() {
+		// TODO Auto-generated method stub
+		return "a label";
+	}
+
+	@Override
+	public Collection<?> getResult() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void redo() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void undo() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- workspace/edu.toronto.cs.openome.evaluation/src/edu/toronto/cs/openome/evaluation/handlers/InteractiveQualReasonerHandler.java
+++ workspace/edu.toronto.cs.openome.evaluation/src/edu/toronto/cs/openome/evaluation/handlers/InteractiveQualReasonerHandler.java
@@ -5,20 +5,32 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.emf.common.command.CommandStack;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
 import org.eclipse.emf.edit.domain.EditingDomain;
-
+import org.eclipse.emf.edit.ui.util.EditUIUtil;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 
+import edu.toronto.cs.openome.evaluation.commands.SetQualitativeEvaluationLabelCommand;
 import edu.toronto.cs.openome_model.EvaluationLabel;
 import edu.toronto.cs.openome_model.Intention;
+//import org.eclipse.gmf.runtime.diagram.ui.resources.editor.parts.DiagramDocumentEditor;
+//import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditorWithFlyOutPalette;
+import edu.toronto.cs.openome_model.diagram.part.Openome_modelDiagramEditor;
 import edu.toronto.cs.openome_model.impl.ModelImpl;
-import edu.toronto.cs.openome_model.presentation.openome_modelEditor;
+//import edu.toronto.cs.openome_model.presentation.openome_modelEditor;
 
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
+import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramCommandStack;
 
 
 
@@ -39,40 +51,91 @@ public class InteractiveQualReasonerHandler implements IHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		
-		openome_modelEditor gme = (openome_modelEditor) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+		System.out.println("Yo");
+		Openome_modelDiagramEditor gme;
+		//DiagramDocumentEditor gme;
+		
+		try {
+			IWorkbenchWindow iww = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+			
+			System.out.println("Got Workbench Window");
+			IWorkbenchPage iwp = iww.getActivePage(); //assume correct page is showing ... dubious
+			System.out.println(iwp.getLabel());
+			IEditorPart iep= iwp.getActiveEditor(); //
+	
+			System.out.println(iep.getTitle());
+		
+			gme = (Openome_modelDiagramEditor) iep; //
+			
+			//gme = (openome_modelEditor) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+		}
+		catch (Exception e) {
+			System.out.println("Exception getting modelEditor");
+			return null;
+		}
 		//IEditor gme = (IEditor) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
 		
 		EditingDomain editingDomain = gme.getEditingDomain();
 		
-		ResourceSet resource;
+		CommandStack dcs = editingDomain.getCommandStack();
 		
-		//try {
-			// Load the resource through the editing domain.
-			//
-			//resourceURI = EditUIUtil.getURI(gme.getEditorInput());
-			//resource = editingDomain.getResourceSet().getResource(resourceURI, true);
-		resource = editingDomain.getResourceSet();
-		//}
-		//catch (Exception e) {
-		//	resource = editingDomain.getResourceSet().getResource(resourceURI, false);
-		//}
+		Resource resource;
+		
+		URI resourceURI;
+		
+//		try {
+//		// Load the resource through the editing domain.
+//			
+//			resourceURI = EditUIUtil.getURI(gme.getEditorInput());
+//			resource = editingDomain.getResourceSet().getResource(resourceURI, true);
+//
+//		}
+//		catch (Exception e) {
+//			resource = editingDomain.getResourceSet().getResource(resourceURI, false);
+//		}
+		
+		ResourceSet resourceSet = editingDomain.getResourceSet();
 		
 		XMIResourceImpl xmires = null;
-		for(Resource tmp: resource.getResources()) {
+		
+		System.out.println(resourceSet.toString());
+				
+		for(Resource tmp: resourceSet.getResources()) {
 			if (tmp instanceof XMIResourceImpl) {
 				xmires = (XMIResourceImpl) tmp;
+				System.out.println(tmp.toString());
+				System.out.println("XMIResourceImpl: " + xmires.getPublicId());
 			}
 		}
 		ModelImpl model = null;
+		
+			
+		System.out.println(xmires.toString());
+		
 		for(EObject tmp2: xmires.getContents()){ 
 			if (tmp2 instanceof ModelImpl) 
 				model = (ModelImpl) tmp2; 
+				System.out.println("ModelImpl: " + model.getName());
+				System.out.println(model.toString());
 		}
 		
-		EList ints = model.getIntentions();
-		for (Object o : ints) {
+		
+		for (Object o : model.getIntentions()) {
 			Intention i = (Intention) o;
-			i.setQualitativeReasoningSatisfiedLabel(EvaluationLabel.SATISFIED);
+			
+			System.out.println(i.toString());
+			System.out.println(i.getName());
+			
+			//i.setQualitativeReasoningSatisfiedLabel(EvaluationLabel.SATISFIED);
+			
+			
+			SetQualitativeEvaluationLabelCommand setLabel = new SetQualitativeEvaluationLabelCommand(i);
+			
+			System.out.println(setLabel.getLabel());
+			
+			dcs.execute(setLabel);
+		
+		
 		}
 		
 		//r = new Reasoning(model, EvalType.InteractiveQualReasoning)
