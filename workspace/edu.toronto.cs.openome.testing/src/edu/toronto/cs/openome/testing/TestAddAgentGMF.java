51f42496fa5d98c84bd2f4e95b9cbe01f07fdc50
johan <johan@1ab8f05b-f909-0410-947c-871aca028256>
2009-07-17 19:24:11 +0000

    added some test cases for Agent
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@1237 1ab8f05b-f909-0410-947c-871aca028256

--- workspace/edu.toronto.cs.openome.testing/src/edu/toronto/cs/openome/testing/TestAddAgentGMF.java
+++ workspace/edu.toronto.cs.openome.testing/src/edu/toronto/cs/openome/testing/TestAddAgentGMF.java
@@ -27,9 +27,10 @@ import edu.toronto.cs.openome_model.Model;
 import edu.toronto.cs.openome_model.diagram.part.Openome_modelDiagramEditorUtil;
 import edu.toronto.cs.openome_model.diagram.providers.Openome_modelElementTypes;
 import edu.toronto.cs.openome_model.impl.ActorImpl;
+import edu.toronto.cs.openome_model.impl.AgentImpl;
 import edu.toronto.cs.openome_model.impl.ModelImpl;
 
-public class TestAddActorGMF{
+public class TestAddAgentGMF{
 	
 	private static Diagram diagram;
 	private static edu.toronto.cs.openome_model.Model model;
@@ -54,40 +55,40 @@ public class TestAddActorGMF{
 	}
 	
 	/**
-	 * Tests that a new actor can be created
+	 * Tests that a new agent can be created
 	 */
 	@Test
-	public void canAddActor() {
-		String name = "testActor";
+	public void canAddAgent() {
+		String name = "testAgent";
 		
 		assertTrue(model.getContainers().isEmpty());
 		
-		Command create = new ActorImplCreateCommand((ModelImpl) model, name);
+		Command create = new AgentImplCreateCommand((ModelImpl) model, name);
 		cs.execute(create);
 		
 		assertFalse(model.getContainers().isEmpty());
 		
 		assertTrue(model.getContainers().size() == 1);
 		
-		assertTrue(model.getContainers().get(0) instanceof ActorImpl);
+		assertTrue(model.getContainers().get(0) instanceof AgentImpl);
 		assertTrue(model.getContainers().get(0).getName().equals(name));
 		
 	}
 	
 	/**
-	 * Tests that a new actor is originally empty
+	 * Tests that a new agent is originally empty
 	 * i.e. contains no intentions
 	 */
 	@Test
-	public void actorIsEmpty(){
+	public void agentIsEmpty(){
 		assertTrue(model.getContainers().get(0).getIntentions().isEmpty());
 	}
 	
 	/**
-	 * Tests that a new actor initially has no connecting line to any other objects
+	 * Tests that a new agent initially has no connecting line to any other objects
 	 */
 	@Test
-	public void actorIsNotLinked(){
+	public void agentIsNotLinked(){
 		assertTrue(model.getContainers().get(0).getAssociationFrom().isEmpty());
 		assertTrue(model.getContainers().get(0).getAssociationTo().isEmpty());
 		assertTrue(model.getContainers().get(0).getDependencyFrom().isEmpty());
