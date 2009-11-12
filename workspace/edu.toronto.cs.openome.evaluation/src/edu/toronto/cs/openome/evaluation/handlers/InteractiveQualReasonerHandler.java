e3e9b4f83b03d71f9ddc91b2c6aa6c22806662b7
Aftab <Aftab@1ab8f05b-f909-0410-947c-871aca028256>
2009-11-12 20:28:12 +0000

    Updated to open AlternatesDialog and add a new alternate
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@1330 1ab8f05b-f909-0410-947c-871aca028256

--- workspace/edu.toronto.cs.openome.evaluation/src/edu/toronto/cs/openome/evaluation/handlers/InteractiveQualReasonerHandler.java
+++ workspace/edu.toronto.cs.openome.evaluation/src/edu/toronto/cs/openome/evaluation/handlers/InteractiveQualReasonerHandler.java
@@ -1,15 +1,27 @@
 package edu.toronto.cs.openome.evaluation.handlers;
 
 //Imports of eclipse stuff
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandlerListener;
 import org.eclipse.emf.common.command.CommandStack;
 import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramCommandStack;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
 
 //Imports of our stuff
+import edu.toronto.cs.openome.evaluation.gui.AlternateDialog;
+import edu.toronto.cs.openome.evaluation.gui.EvalLabelElementTypeLabelProvider;
+import edu.toronto.cs.openome.evaluation.gui.LabelBagElementTypeLabelProvider;
 import edu.toronto.cs.openome.evaluation.qualitativeinteractivereasoning.InteractiveQualReasoner;
 import edu.toronto.cs.openome.evaluation.reasoning.Reasoning;
+import edu.toronto.cs.openome.evaluation.views.AlternativesView;
+import edu.toronto.cs.openome_model.EvaluationLabel;
 import edu.toronto.cs.openome_model.impl.ModelImpl;
 
 
@@ -39,6 +51,28 @@ public class InteractiveQualReasonerHandler extends ReasonerHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 	
+		Shell [] ar = PlatformUI.getWorkbench().getDisplay().getShells();
+		
+		//Open a dialog box for alternative name and description
+		AlternateDialog ad = new AlternateDialog(ar[0]);
+	
+		ad.open();
+		
+		if (ad.getReturnCode() == Window.CANCEL){
+			return null;
+		}
+		
+		//Create a new alternate in the Alternates View
+	
+		AlternativesView av = 
+			(AlternativesView)	PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().findView("edu.toronto.cs.openome.evaluation.views.AlternativesView");
+				
+	
+		av.addAlternative(ad.getName(), ad.getDescription());
+		
+		
+		
+		
 		ModelImpl mi = getModelImpl();
 		CommandStack cs = getCommandStack();
 		
