05b08a5704bebeef8b9f636fe211631d37cf3918
yijun <yijun@1ab8f05b-f909-0410-947c-871aca028256>
2010-02-23 23:17:24 +0000

    remove the obsolete build scripts
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@1365 1ab8f05b-f909-0410-947c-871aca028256

--- win32/eclipse.bat
+++
@@ -1,8 +0,0 @@
-set ECLIPSE_SDK=C:\IDE\sdk\3.4
-del /y configuration
-mkdir configuration
-copy %ECLIPSE_SDK%\eclipse\configuration\config.ini configuration
-rd /s /q %ECLIPSE_SDK%\eclipse\links
-mkdir %ECLIPSE_SDK%\eclipse\links
-copy links\*.* %ECLIPSE_SDK%\eclipse\links
-start /b %ECLIPSE_SDK%\eclipse\eclipse.exe -plugincustomization ..\plugin_customization.ini -configuration configuration -data ..\workspace
