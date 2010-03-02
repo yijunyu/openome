63a117b343a0a99586c45a58353fa0211bdcddbe
yijun <yijun@1ab8f05b-f909-0410-947c-871aca028256>
2010-03-02 13:51:47 +0000

    creating the new 3.5.2 product to include two additional plugins in the dependencies.
    Also change the build script for x86, which is supposed to be the
    same as x86_64 except for the ARCH parameter.
    
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@1374 1ab8f05b-f909-0410-947c-871aca028256

--- x86/build.sh
+++ x86/build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 #-----------------------------------------------------------
-#PRODUCT=core
 PRODUCT=openome_model
-IDE=/home/yijun/IDE
-VERSION=3.5
+VERSION=3.5.2
+IDE=$HOME/IDE
+OS=linux
+WS=gtk
+ARCH=x86
+#ARCH=x86_64
+#-----------------------------------------------------------
 export ECLIPSE_HOME=$IDE/sdk/$VERSION/eclipse
 export BUILD_HOME=$HOME/eclipse.build
 export OPENOME_HOME=$(cd ..; pwd)
-#-----------------------------------------------------------
-PRODUCT_FILE=$PRODUCT.product
+PRODUCT_FILE=$PRODUCT-$VERSION.product
 PROJECT_FILE=$PRODUCT
 \ls ../workspace > workspace.plugins.txt
 xsltproc product.xsl ../workspace/$PROJECT_FILE/$PRODUCT_FILE > product.plugins.txt
@@ -24,11 +27,14 @@ done
 rm -rf $BUILD_HOME/I.openome/*
 cd -
 cp -r $ECLIPSE_HOME/configuration .
-# rm -rf $ECLIPSE_HOME/links
-# mkdir -p $ECLIPSE_HOME/links
-# cp $PRODUCT.links-$VERSION/*.* $ECLIPSE_HOME/links
 cp $ECLIPSE_HOME/plugins/org.eclipse.pde.build_*/scripts/productBuild/productBuild.xml build.xml
-sed -e "s/MYPRODUCT/\/$PROJECT_FILE\/$PRODUCT_FILE/" productBuild.properties  | sed -e "s/MYVERSION/$VERSION/" > build.properties
+sed -e "s/BUILD_PRODUCT/\/$PROJECT_FILE\/$PRODUCT_FILE/" productBuild.properties \
+| sed -e "s#BUILD_IDE#$IDE#" \
+| sed -e "s#BUILD_OS#$OS#" \
+| sed -e "s#BUILD_WS#$WS#" \
+| sed -e "s#BUILD_ARCH#$ARCH#" \
+| sed -e "s/BUILD_VERSION/$VERSION/" \
+> build.properties
 $ECLIPSE_HOME/eclipse -configuration configuration -nosplash -application org.eclipse.ant.core.antRunner -Dbuilder=. $*
 if [ $? ]; then
  exit
