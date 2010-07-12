979d03eedffe971097a07e1d0664c7f66f38343e
Alex <Alex@1ab8f05b-f909-0410-947c-871aca028256>
2010-07-12 03:56:33 +0000

    Various fixes to changing actor types: - no more NullPointer exception - keeps the old actor's intentions - maintains the intentions' original layout within the container
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@1471 1ab8f05b-f909-0410-947c-871aca028256

--- workspace/edu.toronto.cs.openome_model.diagram/src/customsrc/SetActorTypeAction.java
+++ workspace/edu.toronto.cs.openome_model.diagram/src/customsrc/SetActorTypeAction.java
@@ -1,63 +1,42 @@
 package customsrc;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.gef.RootEditPart;
 import org.eclipse.gmf.runtime.common.core.command.CommandResult;
 import org.eclipse.gmf.runtime.common.core.command.ICommand;
 import org.eclipse.gmf.runtime.common.ui.action.AbstractActionHandler;
 import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
+import org.eclipse.gmf.runtime.diagram.ui.commands.SetBoundsCommand;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.AbstractBorderedShapeEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramCommandStack;
 import org.eclipse.gmf.runtime.diagram.ui.parts.IDiagramEditDomain;
-import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
+import org.eclipse.gmf.runtime.emf.core.util.EObjectAdapter;
 import org.eclipse.gmf.runtime.emf.type.core.IElementType;
 import org.eclipse.gmf.runtime.emf.type.core.commands.CreateElementCommand;
 import org.eclipse.gmf.runtime.emf.type.core.commands.DestroyElementCommand;
 import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
 import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
 import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyElementRequest;
+import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbenchPage;
 
 import edu.toronto.cs.openome_model.Container;
 import edu.toronto.cs.openome_model.EvaluationLabel;
-import edu.toronto.cs.openome_model.Intention;
-import edu.toronto.cs.openome_model.diagram.edit.parts.ActorActorCompartmentEditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.ActorEditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.AgentAgentCompartmentEditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.AgentEditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.Goal2EditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.Goal3EditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.Goal4EditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.Goal5EditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.GoalEditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.PositionEditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.PositionPositionCompartmentEditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.Resource2EditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.Resource3EditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.Resource4EditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.Resource5EditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.ResourceEditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.RoleEditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.RoleRoleCompartmentEditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.Softgoal2EditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.Softgoal3EditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.Softgoal4EditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.Softgoal5EditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.SoftgoalEditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.Task2EditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.Task3EditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.Task4EditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.Task5EditPart;
-import edu.toronto.cs.openome_model.diagram.edit.parts.TaskEditPart;
 import edu.toronto.cs.openome_model.diagram.providers.Openome_modelElementTypes;
-import edu.toronto.cs.openome_model.impl.IntentionImpl;
 
 public class SetActorTypeAction extends AbstractActionHandler {
 	
@@ -98,30 +77,116 @@ public class SetActorTypeAction extends AbstractActionHandler {
 			// determine what type of actor it is, then cast it appropriately 
 			// and apply the appropriate label to it
 			
-			GraphicalEditPart part = (GraphicalEditPart) selection.getFirstElement();
+			GraphicalEditPart part = (GraphicalEditPart) selection.getFirstElement();			
 			IDiagramEditDomain partEditDomain = part.getDiagramEditDomain();
 			DiagramCommandStack dcs = partEditDomain.getDiagramCommandStack();
 			
-			doTypeSwitch(actor, dcs, progressMonitor);
+			List<Point> childCoords = new LinkedList<Point>();
+			findChildCoords(part, childCoords);
 			
+			IFigure fig = part.getContentPane();
+			Rectangle coords = fig.getBounds();
+			
+			doTypeSwitch(actor, dcs, progressMonitor, coords, childCoords);
 		}
-		
 	}
 	
-	private void doTypeSwitch(Object originalEditPart, DiagramCommandStack dcs,	IProgressMonitor progressMonitor) {
-		final EObject originalImpl = ((IGraphicalEditPart)originalEditPart).getNotationView().getElement();
-		TransactionalEditingDomain domain = ((IGraphicalEditPart)originalEditPart).getEditingDomain();
+	/*
+	 * Finds intentions inside an actor, and saves their coordinates in a list.
+	 */
+	private void findChildCoords(GraphicalEditPart part, List<Point> childCoords)
+	{
+		for(Object o : part.getChildren()) {
+			// if this is an intention, then save its figure's coordinates
+			if(o instanceof AbstractBorderedShapeEditPart) {
+				GraphicalEditPart g = (GraphicalEditPart)o;
+				IFigure fig = g.getContentPane();
+				Rectangle coords = fig.getBounds();
+				
+				childCoords.add(new Point(coords.x, coords.y));
+			
+			// not an intention, so look deeper
+			} else {
+				findChildCoords((GraphicalEditPart)o, childCoords);
+			}
+		}
+	}
+	
+	private void doTypeSwitch(Object originalEditPart, DiagramCommandStack dcs,	IProgressMonitor progressMonitor, Rectangle coords, List<Point> childCoords) {
+		IGraphicalEditPart gEditPart = (IGraphicalEditPart)originalEditPart;
+		
+		final EObject originalImpl = gEditPart.getNotationView().getElement();
+		TransactionalEditingDomain domain = gEditPart.getEditingDomain();
+		RootEditPart root = gEditPart.getRoot();
 		
 		//Create new element (automatically sync info as well)
 		CreateElementCommand create = selectCreateActorCommand(originalImpl, domain);
 		dcs.execute(new ICommandProxy(create));
-		
-		//Before deleting the old element we should copy the children
 
 		//Delete old element
 		DestroyElementCommand destroy = new DestroyElementCommand(new DestroyElementRequest(domain, originalImpl, false));
 		dcs.execute(new ICommandProxy(destroy));
 		
+		// Place the new container in the old one's spot
+		
+		EObject newObject = create.getCreateRequest().getNewElement();
+		IGraphicalEditPart rootContents = (IGraphicalEditPart)root.getContents();
+		
+		GraphicalEditPart containerPart = null;
+		
+		for(Object o : rootContents.getChildren()) {
+			View v = ((IGraphicalEditPart)o).getNotationView();
+			EObject viewObject = v.getElement();
+			
+			// this is the new element
+			if(viewObject == newObject) {
+				SetBoundsCommand s = new SetBoundsCommand(domain, "", new EObjectAdapter(v), new Point(coords.x, coords.y));
+				dcs.execute(new ICommandProxy(s));
+				
+				containerPart = (GraphicalEditPart)o;
+			}
+		}
+		
+		// Place container's children in their old spots
+		
+		if(containerPart == null) {
+			// problem
+		} else {
+			for(Object i : ((Container)newObject).getIntentions()) {		
+				View v = getIntentionView(containerPart, (EObject)i);
+				
+				if(v == null) {
+					// problem
+				} else {				
+					Point p = childCoords.remove(0);
+					
+					SetBoundsCommand s = new SetBoundsCommand(domain, "", new EObjectAdapter(v), p);
+					dcs.execute(new ICommandProxy(s));
+				}
+			}
+		}
+	}
+	
+	/*
+	 * Gets the EObject's view.
+	 */
+	private View getIntentionView(GraphicalEditPart part, EObject e)
+	{
+		View v = part.getNotationView();
+		
+		if(v.getElement() == e) {
+			return v;
+		} 
+		
+		for(Object o : part.getChildren()) {
+			v = getIntentionView((GraphicalEditPart)o, e);
+			
+			if(v != null) {
+				return v;
+			}
+		}
+			
+		return null;
 	}
 
 	private CreateElementCommand selectCreateActorCommand(
@@ -129,16 +194,16 @@ public class SetActorTypeAction extends AbstractActionHandler {
 		CreateElementRequest req = null;
 		
 		if (changeTo.equals("Actor")){
-			req = new CreateElementRequest(domain, originalImpl.eContainer(), Openome_modelElementTypes.Actor_1001);
+			req = new CreateElementRequest(domain, originalImpl.eContainer(), Openome_modelElementTypes.Actor_2001);
 		}
 		else if (changeTo.equals("Agent")){
-			req = new CreateElementRequest(domain, originalImpl.eContainer(), Openome_modelElementTypes.Agent_1002);
+			req = new CreateElementRequest(domain, originalImpl.eContainer(), Openome_modelElementTypes.Agent_2002);
 		}
 		else if (changeTo.equals("Position")){
-			req = new CreateElementRequest(domain, originalImpl.eContainer(), Openome_modelElementTypes.Position_1003);
+			req = new CreateElementRequest(domain, originalImpl.eContainer(), Openome_modelElementTypes.Position_2003);
 		}
 		else if (changeTo.equals("Role")){
-			req = new CreateElementRequest(domain, originalImpl.eContainer(), Openome_modelElementTypes.Role_1004);
+			req = new CreateElementRequest(domain, originalImpl.eContainer(), Openome_modelElementTypes.Role_2004);
 		}
 		return new CreateNewActorTypeCommand(req, originalImpl);
 	}
@@ -192,8 +257,7 @@ public class SetActorTypeAction extends AbstractActionHandler {
 	        ((Container) newElement).setName(((Container) oldElement).getName());
 	        
 	        // Now we should also copy children 
-	        
-	        
+	        ((Container) newElement).getIntentions().addAll(((Container) oldElement).getIntentions());
 	        
 	        // Put the newly created element in the request so that the
 	        // 'after' commands have access to it.
