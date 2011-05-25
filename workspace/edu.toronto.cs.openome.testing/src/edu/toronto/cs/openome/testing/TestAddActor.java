2fb857b492baf2ddff331b756249e93496a73cde
denys <denys@1ab8f05b-f909-0410-947c-871aca028256>
2011-05-25 15:34:54 +0000

    Added some actor-adding tests.
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@1566 1ab8f05b-f909-0410-947c-871aca028256

--- workspace/edu.toronto.cs.openome.testing/src/edu/toronto/cs/openome/testing/TestAddActor.java
+++ workspace/edu.toronto.cs.openome.testing/src/edu/toronto/cs/openome/testing/TestAddActor.java
@@ -1,46 +1,81 @@
 package edu.toronto.cs.openome.testing;
 
+import static org.eclipse.swtbot.swt.finder.matchers.WidgetMatcherFactory.allOf;
+import static org.eclipse.swtbot.swt.finder.matchers.WidgetMatcherFactory.widgetOfType;
+import static org.eclipse.swtbot.swt.finder.matchers.WidgetMatcherFactory.withRegex;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
-import org.eclipse.swtbot.eclipse.finder.SWTEclipseBot;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gmf.runtime.notation.impl.DecorationNodeImpl;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEditor;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
+import org.eclipse.swtbot.eclipse.gef.finder.SWTGefBot;
+import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditPart;
+import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditor;
+import org.eclipse.swtbot.swt.finder.SWTBotAssert;
+import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.hamcrest.Matcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import edu.toronto.cs.openome_model.Actor;
+import edu.toronto.cs.openome_model.diagram.edit.parts.ActorEditPart;
+import edu.toronto.cs.openome_model.diagram.edit.parts.ActorEditPart.ActorFigure;
+import edu.toronto.cs.openome_model.impl.ActorImpl;
+
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class TestAddActor{
 	
-	private static SWTEclipseBot bot;
-	 
+	private static SWTBotGefEditor editor;
+	private static Actor actModel;
+	
 	@BeforeClass
 	public static void beforeClass() throws Exception {
 		TestUtil.initializeWorkspace();
-		bot = new SWTEclipseBot();
+		editor = new SWTGefBot().gefEditor("test.ood");
 	}
- 
  
 	@Test
 	public void canAddActor() throws Exception {
-//		System.out.println("The views are: ");
-//		for (SWTBotView view : bot.views()){
-//			System.out.println(view.getTitle());
-//		}
-
-		
-		
+		try {
+			String name = "TestActor";
+			// Draw an actor
+			editor.activateTool("Actor");
+			editor.click(0, 0);
+			editor.directEditType(name);
+			
+			SWTBotGefEditPart actor = editor.getEditPart(name);
+			
+			// check if the model was made correctly
+			DecorationNodeImpl decnode = (DecorationNodeImpl) actor.part().getModel();
+			actModel = (Actor) decnode.getElement();
+			assert(actor.part().getModel() instanceof Actor);
+			
+			assert(actModel.getIntentions().isEmpty());
+			assert(actor.children().isEmpty());
+			
+			// Make sure no connections exist
+			assert(actor.sourceConnections().isEmpty());
+			assert(actor.targetConnections().isEmpty());
+			editor.editParts((Matcher<? extends EditPart>) allOf(withRegex(".*")));
+		} catch (WidgetNotFoundException e) {
+			fail("Could not add a new actor.");
+		}
 	}
- 
- 
+	
 	@AfterClass
-	public static void sleep() {
-		bot.sleep(2000);
+	public static void afterClass() {
+		editor.clear();
 	}
 	
 }
