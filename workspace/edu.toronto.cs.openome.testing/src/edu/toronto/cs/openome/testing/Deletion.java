b0f6ace942a7c2d850a7ece931792c3107f574ad
denys <denys@1ab8f05b-f909-0410-947c-871aca028256>
2011-06-15 15:40:21 +0000

    Created tests to test the deletion of objects within a plain diagram (i.e not in a compartement)
    git-svn-id: https://se.cs.toronto.edu/svn/trac/ome/trunk@1590 1ab8f05b-f909-0410-947c-871aca028256

--- workspace/edu.toronto.cs.openome.testing/src/edu/toronto/cs/openome/testing/Deletion.java
+++ workspace/edu.toronto.cs.openome.testing/src/edu/toronto/cs/openome/testing/Deletion.java
@@ -1,20 +1,11 @@
 package edu.toronto.cs.openome.testing;
 
-import static org.eclipse.swtbot.swt.finder.SWTBotAssert.assertVisible;
 import static org.junit.Assert.*;
 
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
-
-import org.eclipse.gef.EditPart;
-import org.eclipse.gef.EditPartViewer;
-import org.eclipse.gmf.runtime.notation.impl.DiagramImpl;
-import org.eclipse.swt.SWT;
-import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
 import org.eclipse.swtbot.eclipse.gef.finder.SWTGefBot;
 import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditPart;
 import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditor;
-import org.eclipse.swtbot.swt.finder.keyboard.Keyboard;
 import org.eclipse.swtbot.swt.finder.keyboard.KeyboardFactory;
 import org.eclipse.swtbot.swt.finder.keyboard.Keystrokes;
 import org.junit.After;
@@ -24,30 +15,58 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import edu.toronto.cs.openome_model.Model;
-import edu.toronto.cs.openome_model.impl.ModelImpl;
+import edu.toronto.cs.openome_model.diagram.edit.parts.ActorEditPart;
+import edu.toronto.cs.openome_model.diagram.edit.parts.GoalEditPart;
+import edu.toronto.cs.openome_model.diagram.edit.parts.ResourceEditPart;
+import edu.toronto.cs.openome_model.diagram.edit.parts.RoleEditPart;
 
-public class Deletion {
+public class Deletion  {
     private static SWTBotGefEditor editor;
-    private static Keyboard keyboard;
+    private static SWTBotGefEditPart diagram;
+    private static Model model;
     // Different types of delete, as specified by doDelete()
     private static String[] typesOfDelete = {"delete", "backspace", "context"};
+    private static String[][] allLinks = { TestUtil.hardlinks, TestUtil.contributions};
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
         TestUtil.initializeWorkspace();
-        editor = new SWTGefBot().gefEditor("test.ood");
-        keyboard = KeyboardFactory.getDefaultKeyboard(editor.getWidget(), null);
     }
     
     @Before
     public void setUpBeforeTest() throws Exception {
-        //TODO
+        TestUtil.createAndOpenFile();
+        editor = new SWTGefBot().gefEditor("test.ood");
+        diagram = editor.mainEditPart();
+        model = TestUtil.getModel(editor);
+    }
+    
+    @After
+    public void tearDownAfterTest() throws Exception {
+        TestUtil.closeAndDeleteFile();
+    }
+    
+    @Test
+    public void cannotDeleteNothing() throws InterruptedException {
+        
+        for (String type : typesOfDelete) {
+            // Try to do a simple delete
+            doDelete(type);
+            
+            // check that we can still add things and everything works more-or-less correctly.
+            editor.activateTool("Softgoal");
+            editor.click(0, 0);
+            editor.directEditType("a");
+            
+            assertTrue(type.concat(": Could add to the diagram"), diagram.children().size() == 1);
+            assertTrue(type.concat(": Could add to the model"), model.getIntentions().size() == 1);
+            // remove that element, so that the other tests can do their job.
+            doDelete(type);
+        }
     }
     
     @Test
     public void canDeleteOneActor() throws Exception {
-        SWTBotGefEditPart diagram = editor.mainEditPart();
-        Model model = TestUtil.getModel(editor);
         String name;
         
         assertTrue("Diagram is empty", diagram.children().isEmpty());
@@ -76,8 +95,6 @@ public class Deletion {
 
     @Test
     public void canDeleteOneIntention() throws Exception {
-        SWTBotGefEditPart diagram = editor.mainEditPart();
-        Model model = TestUtil.getModel(editor);
         String name;
         
         assertTrue("Diagram is empty", diagram.children().isEmpty());
@@ -105,9 +122,7 @@ public class Deletion {
     }
     
     @Test
-    public void canDeleteMultipleIntentions() throws Exception {
-        SWTBotGefEditPart diagram = editor.mainEditPart();
-        Model model = TestUtil.getModel(editor);
+    public void canDeleteMultipleIntentionsIndependently() throws Exception {
         String name;
         int count = 0;
 
@@ -145,6 +160,216 @@ public class Deletion {
         }
     }
     
+    @Test
+    public void canDeleteMultipleIntentionsSimultaneously() throws Exception {
+        SWTBotGefEditPart diagram = editor.mainEditPart();
+        Model model = TestUtil.getModel(editor);
+        String name;
+
+        assertTrue("Diagram is empty", diagram.children().isEmpty());
+
+        // loop over every delete type.
+        for (String type : typesOfDelete) {
+            int count = 0;
+
+            // loop over every combination of intentions and add it.
+            for (String tool : TestUtil.intentions) {
+                name = tool;
+                count++;
+                
+                // create one intention
+                editor.activateTool(tool);
+                editor.click(count*100, count*100);
+                editor.directEditType(name);
+
+
+                assertTrue(type.concat(": Element was created"), model.getIntentions().size() == count);
+                assertTrue(type.concat(": Diagram does contain an element"), diagram.children().size() == count);
+            }
+            
+            // Select all of the intentions at once, and delete them.
+            editor.drag(0, 0, 1000, 1000);
+            assertFalse(type.concat(": All the intentions were selected"), editor.selectedEditParts().isEmpty());
+            doDelete(type);
+
+            assertTrue(type.concat(": Diagram is empty."), diagram.children().isEmpty());
+            assertTrue(type.concat(": Model is empty."), model.getIntentions().isEmpty());
+        }
+    }
+    
+    @Test
+    public void canDeleteLinks() throws InterruptedException {
+        assertTrue("Diagram is empty", diagram.children().isEmpty());
+        
+        // create two intentions
+        String name = "Hardgoal";
+        editor.activateTool(name).click(1, 1);
+        editor.directEditType(name);
+        name = "Resource";
+        editor.activateTool(name).click(300, 300);
+        editor.directEditType(name);
+        GoalEditPart hardgoal = (GoalEditPart) editor.getEditPart("Hardgoal").part().getParent();
+        ResourceEditPart resource = (ResourceEditPart) editor.getEditPart("Resource").part().getParent();
+
+        assertTrue("Elements in model", model.getIntentions().size() == 2);
+        assertTrue("Elements in diagram", diagram.children().size() == 2);
+        
+        for (String[] links : allLinks) {
+        for (String tool : links) {
+            for (String type : typesOfDelete) {
+                // create a link between the intentions
+                editor.activateTool(tool);
+                editor.drag("Resource", 3, 3);
+                
+                // test is running too quick, not enough time to propagate to model.
+                Thread.sleep(500);
+                assertTrue(type + ": Link in model", model.getDecompositions().size() == 1 
+                        || model.getDependencies().size() == 1
+                        || model.getContributions().size() == 1);
+                assertTrue(type + ": Link in diagram", resource.getSourceConnections().size() == 1 && hardgoal.getTargetConnections().size() == 1);
+                
+                doDelete(type);
+                
+                Thread.sleep(500);
+                assertTrue(type + ": Link not in model", model.getDecompositions().isEmpty() 
+                        || model.getDependencies().isEmpty()
+                        || model.getContributions().isEmpty());
+                assertTrue(type + ": Link not in diagram", resource.getSourceConnections().isEmpty() && hardgoal.getTargetConnections().isEmpty());
+            }
+        }
+        }
+        editor.drag(0, 0, 1000, 1000);
+        doDelete("");
+    }
+    
+    @Test
+    public void canDeleteLinksWhenDeletingIntentions() throws InterruptedException {
+        assertTrue("Diagram is empty", diagram.children().isEmpty());
+        
+        // create two intentions
+        String resName = "Resource";
+        editor.activateTool(resName).click(300, 300);
+        editor.directEditType(resName);
+        ResourceEditPart resource = (ResourceEditPart) editor.getEditPart(resName).part().getParent();
+
+
+        for (String[] links : allLinks) {
+        for (String linkTool : links) {
+            for (String type : typesOfDelete) {
+                String intentionTool = "Hardgoal";
+                editor.activateTool(intentionTool).click(1, 1);
+                editor.directEditType(intentionTool);
+                AbstractGraphicalEditPart intention = (AbstractGraphicalEditPart) editor.getEditPart(intentionTool).part().getParent();
+                
+                assertTrue("Elements in model", model.getIntentions().size() == 2);
+                assertTrue("Elements in diagram", diagram.children().size() == 2);
+                
+                // create a link between the intentions
+                editor.activateTool(linkTool);
+                editor.drag(resName, 3, 3);
+                
+                // test is running too quick, not enough time to propagate to model.
+                Thread.sleep(500);
+                assertTrue(type + ": Link in model", model.getDecompositions().size() == 1 
+                        || model.getDependencies().size() == 1
+                        || model.getContributions().size() == 1);
+                assertTrue(type + ": Link in diagram", resource.getSourceConnections().size() == 1 && intention.getTargetConnections().size() == 1);
+                
+                editor.getEditPart(intentionTool).click();
+                doDelete(type);
+                
+                Thread.sleep(500);
+                assertTrue(type + ": Link not in model", model.getDecompositions().isEmpty() 
+                        || model.getDependencies().isEmpty()
+                        || model.getContributions().isEmpty());
+                assertTrue(type + ": Link not in diagram", resource.getSourceConnections().isEmpty());
+            }
+        }
+        }
+        editor.drag(0, 0, 1000, 1000);
+        doDelete("");
+    }
+    
+    @Test
+    public void canDeleteAssociations() throws InterruptedException {
+        assertTrue("Diagram is empty", diagram.children().isEmpty());
+        
+        // create two intentions
+        String name = "Actor";
+        editor.activateTool(name).click(1, 1);
+        editor.directEditType(name);
+        name = "Role";
+        editor.activateTool(name).click(500, 500);
+        editor.directEditType(name);
+        ActorEditPart actor = (ActorEditPart) editor.getEditPart("Actor").part().getParent();
+        RoleEditPart role = (RoleEditPart) editor.getEditPart("Role").part().getParent();
+
+        assertTrue("Elements in model", model.getContainers().size() == 2);
+        assertTrue("Elements in diagram", diagram.children().size() == 2);
+        
+        for (String tool : TestUtil.associations) {
+            for (String type : typesOfDelete) {
+                // create a link between the intentions
+                editor.activateTool(tool);
+                editor.drag("Role", 3, 3);
+                
+                // test is running too quick, not enough time to propagate to model.
+                Thread.sleep(600);
+                assertTrue(type + ": Link in model", model.getAssociations().size() == 1);
+                assertTrue(type + ": Link in diagram", role.getSourceConnections().size() == 1 && actor.getTargetConnections().size() == 1);
+                
+                doDelete(type);
+                
+                Thread.sleep(600);
+                assertTrue(type + ": Link not in model", model.getAssociations() .isEmpty());
+                assertTrue(type + ": Link not in diagram", role.getSourceConnections().isEmpty() && actor.getTargetConnections().isEmpty());
+            }
+        }
+        editor.drag(0, 0, 1000, 1000);
+        doDelete("");
+    }
+    
+    @Test
+    public void canDeleteAssociationsWhenDeletingActors() throws InterruptedException {
+        assertTrue("Diagram is empty", diagram.children().isEmpty());
+        
+        // create two intentions
+        String resName = "Role";
+        editor.activateTool(resName).click(500, 500);
+        editor.directEditType(resName);
+        RoleEditPart role = (RoleEditPart) editor.getEditPart(resName).part().getParent();
+
+
+        for (String linkTool : TestUtil.associations) {
+            for (String type : typesOfDelete) {
+                String actorTool = "Actor";
+                editor.activateTool(actorTool).click(1, 1);
+                editor.directEditType(actorTool);
+                AbstractGraphicalEditPart intention = (AbstractGraphicalEditPart) editor.getEditPart(actorTool).part().getParent();
+                
+                assertTrue("Elements in model", model.getContainers().size() == 2);
+                assertTrue("Elements in diagram", diagram.children().size() == 2);
+                
+                // create a link between the intentions
+                editor.activateTool(linkTool);
+                editor.drag(resName, 10, 10);
+                
+                // test is running too quick, not enough time to propagate to model.
+                Thread.sleep(500);
+                assertTrue(type + ": Link in model", model.getAssociations().size() == 1); 
+                assertTrue(type + ": Link in diagram", role.getSourceConnections().size() == 1 && intention.getTargetConnections().size() == 1);
+                
+                editor.getEditPart(actorTool).click();
+                doDelete(type);
+                
+                Thread.sleep(500);
+                assertTrue(type + ": Link not in model", model.getAssociations().isEmpty());
+                assertTrue(type + ": Link not in diagram", role.getSourceConnections().isEmpty());
+            }
+        }
+        editor.drag(0, 0, 1000, 1000);
+        doDelete("");
+    }
     /**
      * Do a deletion, depending on the type. If the type is 
      * "delete": press the delete key, 
@@ -163,15 +388,4 @@ public class Deletion {
             editor.clickContextMenu("Delete from Model");
         }
     }
-    
-    @After
-    public void tearDownAfterTest() throws Exception {
-        //TODO
-    }
-    
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-        editor.close();
-    }
-
 }
